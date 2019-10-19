e5a9d3d4fc44e8aab5ad4dc9c4155688721da87c
Mo Harris <moharrisstl@gmail.com>
2019-10-19 16:20:06 -0500

    Initial commit

---
+++ src/main/java/org/launchcode/safehouse/SafeHouseApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.safehouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafeHouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SafeHouseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/launchcode/safehouse/SafeHouseApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.safehouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafeHouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
