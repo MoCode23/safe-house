9b6270c8268d9349d59e513a97f610ee4d0844c0
Mo Harris <moharrisstl@gmail.com>
2019-10-28 19:05:20 -0500

    create add house button

--- src/main/java/org/launchcode/safehouse/controllers/HouseController.java
+++ src/main/java/org/launchcode/safehouse/controllers/HouseController.java
@@ -3,6 +3,8 @@ package org.launchcode.safehouse.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.ArrayList;
@@ -11,14 +13,25 @@ import java.util.ArrayList;
 @RequestMapping("safehouse")                    // request path: /safehouse
 public class HouseController {
 
+    static ArrayList<String> houses = new ArrayList<>();
+
     @RequestMapping(value = "")
     public String index(Model model) {
-
-        ArrayList<String> houses = new ArrayList<>();
-        houses.add("saint louis");
 
         model.addAttribute("houses", houses);
         model.addAttribute("title", "Safe House");
         return "house/index";
+    }
+
+    @RequestMapping(value = "add", method = RequestMethod.GET)
+    public String displayAddHouseForm(Model model){
+        model.addAttribute("title", "Add House");
+        return "house/add";
+    }
+
+    @RequestMapping(value = "add", method = RequestMethod.POST)
+    public String processAddHouseForm(@RequestParam String houseZip){
+        houses.add(houseZip);
+        return "redirect:";
     }
 }
